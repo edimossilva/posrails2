a3f03e0e50070004fc119b23231effbb11a75f12
Edimo Silva <edimo.silva@redealumni.com>
2019-04-28 10:06:22 -0300

    Adding tests to create and creating service

--- spec/controllers/api/v1/pictures_controller_spec.rb
+++ spec/controllers/api/v1/pictures_controller_spec.rb
@@ -44,16 +44,16 @@ RSpec.describe Api::V1::PicturesController, type: :controller do
       expect(response.body).to eq(pictures)
     end
   end
+
   describe '#create' do
     let!(:name) { 'nome qualquer' }
     let!(:image_name) { 'naruto.jpeg' }
     let!(:file_path) { Rails.root.join('spec', 'support', 'assets', image_name) }
-    let!(:image) { fixture_file_upload(file_path, 'image/jpeg') }
-
-    before do
-      post :create, params: { name: name, image: image }
-    end
+    let!(:valid_image) { fixture_file_upload(file_path, 'image/jpeg') }
     context 'When image is created' do
+      before do
+        post :create, params: { name: name, image: valid_image }
+      end
       it 'returns :created' do
         expect(response).to have_http_status(:created)
       end
@@ -69,6 +69,38 @@ RSpec.describe Api::V1::PicturesController, type: :controller do
         body = response.body
         body_image_name = JSON(body)['image_url'].split('/').last
         expect(body_image_name).to eq(image_name)
+      end
+    end
+    context 'When image is not create' do
+      context 'When name is empty' do
+        let!(:empty_name) { '' }
+        before do
+          post :create, params: { name: empty_name, image: valid_image }
+        end
+        it 'returns :unprocessable_entity' do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+        it 'contains name cant be blank error message' do
+          body = response.body
+          name_errors = JSON(body)['errors']['name']
+          expect(name_errors).to include("can't be blank")
+        end
+      end
+      context 'When name has been taken' do
+        let!(:picture) { create :picture, :with_image }
+        let!(:taken_name) { picture.name }
+
+        before do
+          post :create, params: { name: taken_name, image: valid_image }
+        end
+        it 'returns :unprocessable_entity' do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+        it 'contains name has already been taken error message' do
+          body = response.body
+          name_errors = JSON(body)['errors']['name']
+          expect(name_errors).to include('has already been taken')
+        end
       end
     end
   end
